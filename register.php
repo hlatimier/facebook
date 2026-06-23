f944bb7aa4ae9d3f27c4efb4eae4d2c3429ef3a8
hlatimier <38042189+hlatimier@users.noreply.github.com>
2026-06-23 16:10:15 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,18 +1,4 @@
-<?php
-require 'config.php';
 
-if($_POST){
-
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-    $mdp = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
-
-    $req = $pdo->prepare("
-        INSERT INTO utilisateurs(nom,email,mot_de_passe)
-        VALUES(?,?,?)
-    ");
-
-    $req->execute([$nom,$email,$mdp]);
 
     header("Location: login.php");
 }
