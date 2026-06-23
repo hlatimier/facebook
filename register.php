f81c5d4550d01ec5a1f50d5bac70a3751218bf3f
hlatimier <38042189+hlatimier@users.noreply.github.com>
2026-06-23 16:10:34 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,13 +1,1 @@
 
-
-    header("Location: login.php");
-}
-?>
-
-<form method="post">
-<h2>Inscription</h2>
-<input name="nom" placeholder="Nom"><br>
-<input name="email" placeholder="Email"><br>
-<input name="mot_de_passe" type="password" placeholder="Mot de passe"><br>
-<button>S'inscrire</button>
-</form>
