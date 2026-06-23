055dbc247d649ad4e6c365ebf7f30f320cb75244
hlatimier <38042189+hlatimier@users.noreply.github.com>
2026-06-23 16:07:55 +0200

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,15 +1,1 @@
-<?php
-require 'config.php';
 
-$req = $pdo->prepare("
-INSERT INTO commentaires(publication_id,utilisateur_id,contenu)
-VALUES(?,?,?)
-");
-
-$req->execute([
-    $_POST['id'],
-    $_SESSION['user_id'],
-    $_POST['contenu']
-]);
-
-header("Location: index.php");
