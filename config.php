abf7e41aa7e4405a9df8b61e1d36a1092d3b12ec
hlatimier <38042189+hlatimier@users.noreply.github.com>
2026-06-23 16:08:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,1 @@
-<?php
-session_start();
 
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=socialbook;charset=utf8mb4",
-    "root",
-    ""
-);
-
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
